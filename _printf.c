fcad12bcfe2b141d88c940d9077402d3a6abe421
njeeri <michellenjeeeri@gmail.com>
2023-07-29 04:58:02 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,55 +1,44 @@
 #include "main.h"
-void print_buffer(char buffer[], int *buff_ind);
 /**
- * _printf - Fuction
- * @format: pointer
- * Return: Printed chars.
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for
+ * Return: the length of the string
  */
 int _printf(const char *format, ...)
 {
-int i, printed = 0, printed_chars = 0;
-int flags, width, precision, size, buff_ind = 0;
-va_list list;
-char buffer[BUFF_SIZE];
-if(format == NULL)
+convert_match m[] = {
+{"%s", printf_string}, {"%c", printf_char},
+{"%%", printf_37},
+{"%i", printf_int}, {"%d", printf_doc}, {"%r", printf_srev},
+{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
+{"%o", printf_oct}, {"%x", print_hex}, {"%X", printf_HEX},
+{"%S", printf_exclusive_string}, {"%p", printf_pointer}
+};
+va_list args;
+int i, j, len;
+va_start(args, format);
+if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 return (-1);
-va_start(list, format);
-for (i = 0; format && format[i] != '\0'; i++)
+i = 0;
+len = 0;
+while (format[i] != '\0')
 {
-if (format[i] != '%')
+for (j = 13; j >= 0; j--)
 {
-buffer[buff_ind++] = format[i];
-if (buff_ind == BUFF_SIZE)
-print_buffer(buffer, &buff_ind);
-/* write(1, &format[i], 1); */
-printed_chars++;
-}
-else
+if (m[j].id[0] == format[i] && m[j].id[i] == format[i + 1])
 {
-print_buffer(buffer, &buff_ind);
-flags = get_flags(format, &i);
-width = get_width(format, &i, list);
-precision = get_precision(format, &i, list);
-size = get_size(format, &i);
-++i;
-printed = handle_print(format, &i, list, buffer, flags, width, precision, size);
-if (printed == -1)
-return (-1);
-printed_chars += printed;
+len += m[j].f(args);
+i = i + 2;
+break;
 }
 }
-print_buffer(buffer, &buff_ind);
-va_end(list);
-return (printed_chars);
-}
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
+if (j < 0)
 {
-if (*buff_ind > 0)
-write(1, &buffer[0], *buff_ind);
-*buff_ind = 0;
+_putchar(format[i]);
+len++;
+i++;
+}
+}
+va_end(args);
+return (len);
 }
